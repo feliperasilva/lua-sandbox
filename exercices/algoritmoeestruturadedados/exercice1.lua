3b26676fa1deac2471dc736b08902759815c8b88
feliperasilva <fras2@discente.ifpe.edu.br>
2025-09-18 20:01:28 -0300

    feat: exercise 1 from "algoritmos e estrutura de dados"

---
+++ exercices/algoritmoeestruturadedados/exercice1.lua
@@ -0,0 +1,16 @@
+
+
+function potencia(base, expoente)
+    if expoente > 0 then
+        return base * potencia(base, expoente - 1)
+    elseif expoente < 0 then
+        return 1 / potencia(base, -expoente)
+    else
+        return 1
+    end
+end
+
+print(potencia(2, 2))
+print(potencia(3, 2))
+print(potencia(4, 4))
+print(potencia(2, -2))
